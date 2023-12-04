df57cabc76737a35cf2ac68af178ade210298d01
Neverlanders <vanhoutte.tom@gmail.com>
2023-12-04 12:14:31 +0100

    push

--- src/views/ConcertenView.vue
+++ src/views/ConcertenView.vue
@@ -1,61 +1,56 @@
 <script setup>
-   import ConcertCard from "@/components/ConcertCard.vue";
-   import {ref, onMounted} from 'vue'
-   import ConcertenService from "../../services/ConcertenService";
-   import {watchEffect} from "vue";
-   //array van objecten
-   const concerten = ref(null)
-   const isLaatstePagina = ref(false)
-   const concertenPerPagina = ref(3)
-   const totaalAantalPaginas= ref(0)
-   const totaalAantalConcerten = ref(0)
-   const props=defineProps({
-     page:Number
-   })
+import ConcertCard from "@/components/ConcertCard.vue";
+import {ref, onMounted} from 'vue'
+import ConcertenService from "../../services/ConcertenService";
+import {watchEffect} from "vue";
+//array van objecten
+const concerten = ref(null)
+const isLaatstePagina = ref(false)
+const concertenPerPagina = ref(3)
+const totaalAantalPaginas = ref(0)
+const totaalAantalConcerten = ref(0)
+const props = defineProps({
+  page: Number
+})
+const updateConcerten = () => {
+  ConcertenService.getConcerten(concertenPerPagina.value, props.page)
+      .then((response) => {
+        concerten.value = response.data;
+        isLaatstePagina.value = response.data.length < concertenPerPagina.value;
+        totaalAantalPaginas.value = Math.ceil(totaalAantalConcerten.value / concertenPerPagina.value);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+};
 
-   onMounted(()=>{
-     ConcertenService.getAllConcerten()
-         .then((response)=>{
-           totaalAantalPaginas.value = Math.ceil(response.data.length / concertenPerPagina.value)
-           totaalAantalConcerten.value = response.data.length
-         }).catch((error)=> {
-       console.log(error)
-     });
-     watchEffect(()=>{
-       ConcertenService.getConcerten(concertenPerPagina.value,props.page)
-           .then((response)=>{
-             concerten.value = response.data
-             isLaatstePagina.value = response.data.length < concertenPerPagina.value
-           }).catch((error)=>{
-         console.log(error)
-       })
-     })
+onMounted(() => {
+  ConcertenService.getAllConcerten()
+      .then((response) => {
+        totaalAantalPaginas.value = Math.ceil(response.data.length / concertenPerPagina.value)
+        totaalAantalConcerten.value = response.data.length
+      }).catch((error) => {
+    console.log(error)
+  });
+  watchEffect(() => {
+    updateConcerten();
+  })
+})
 
-   })
-
-   const selectAantal = (aantal)=>{
-    concertenPerPagina.value = aantal;
-    /* props.page = 1; // Reset de huidige pagina naar 1*/
-
-     ConcertenService.getConcerten(concertenPerPagina.value, props.page)
-         .then((response) => {
-           concerten.value = response.data;
-           isLaatstePagina.value = response.data.length < concertenPerPagina.value;
-           totaalAantalPaginas.value = Math.ceil(totaalAantalConcerten.value / concertenPerPagina.value);
-         })
-         .catch((error) => {
-           console.log(error);
-         });
-      }
+const selectAantal = (aantal) => {
+  concertenPerPagina.value = aantal;
+  updateConcerten();
+}
 </script>
 
 <template>
   <div class="home">
     <div class="dropdown text-center">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+      <button id="dropdownMenuButton" aria-expanded="false" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
+              type="button">
         #Items per pagina
       </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <ul aria-labelledby="dropdownMenuButton" class="dropdown-menu">
         <li v-for="items in totaalAantalConcerten" :key="items">
           <a class="dropdown-item" href="#" @click.prevent="selectAantal(items)">
             {{ items }}
@@ -68,18 +63,19 @@
     </div>
     <ul class="pagination d-flex justify-content-center">
       <!--        vorige link-->
-      <li class="page-item" v-if="page !== 1">
-        <RouterLink class="page-link"  :to="{name:'concerten',query:{page:page-1}}">Vorige</RouterLink>
+      <li v-if="page !== 1" class="page-item">
+        <RouterLink :to="{name:'concerten',query:{page:page-1}}" class="page-link">Vorige</RouterLink>
       </li>
-<!--      dynamische paginanummers-->
-      <li class="page-item" v-for="pageNum in totaalAantalPaginas" :key="pageNum">
-        <RouterLink class="page-link" :class="{ 'active': page === pageNum }"
-          :to="{name:'concerten',query:{page:pageNum}}">{{pageNum}}</RouterLink>
+      <!--      dynamische paginanummers-->
+      <li v-for="pageNum in totaalAantalPaginas" :key="pageNum" class="page-item">
+        <RouterLink :class="{ 'active': page === pageNum }" :to="{name:'concerten',query:{page:pageNum}}"
+                    class="page-link">{{ pageNum }}
+        </RouterLink>
       </li>
 
       <!--        volgende link-->
-      <li class="page-item" v-if="!isLaatstePagina">
-        <RouterLink class="page-link" :to="{name:'concerten',query:{page:page+1}}">Next</RouterLink>
+      <li v-if="!isLaatstePagina" class="page-item">
+        <RouterLink :to="{name:'concerten',query:{page:page+1}}" class="page-link">Next</RouterLink>
       </li>
     </ul>
   </div>
