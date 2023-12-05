8c1464f45263380c03f0ddb2f716244605b2ab79
Neverlanders <vanhoutte.tom@gmail.com>
2023-12-05 11:58:00 +0100

    push

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 
 
-import { createApp } from 'vue'
+import { createApp,reactive } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const myAlert = reactive({alertBericht:''})
+
+const app = createApp(App).provide('myAlertVar',myAlert)
 
 app.use(createPinia())
 app.use(router)
--- src/views/concert/ConcertRegisterView.vue
+++ src/views/concert/ConcertRegisterView.vue
@@ -1,12 +1,29 @@
 <script setup>
-
-defineProps({
+import {inject, onBeforeMount, ref} from 'vue';
+const myAlert = inject('myAlertVar');
+const isGeregistreerd = ref(false);
+const props=defineProps({
   concert:{
     required:true,
   }
 })
+const register =()=>{
+  if(isGeregistreerd.value){
+    myAlert.alertBericht = 'U bent AL geregistreerd voor ' + props.concert.title
+  }else{
+    myAlert.alertBericht = 'U heeft zich geregistreerd voor ' + props.concert.title
+    isGeregistreerd.value=true
+  }
+}
+onBeforeMount(()=>{
+  myAlert.alertBericht
+})
 
 </script>
 <template>
-  <p>{{ concert.title }} geregistreerd</p>
+  <div class="alert alert-success" v-if="myAlert.alertBericht">
+    {{myAlert.alertBericht}}
+  </div>
+  <p>{{ concert.title }} hier registreren</p>
+  <button class="btn btn-success" @click="register">Register</button>
 </template>
